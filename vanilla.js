c106c774dafb40627f805d685604222190ea9f2a
sosally <thdud4143@gmail.com>
2023-09-07 17:38:56 +0900

    0907_mo_gallery_flow

--- vanilla.js
+++ vanilla.js
@@ -23,10 +23,60 @@ let globalDepth = document.querySelector('.global_depth');
 globalIcon.addEventListener('click', function(){
     globalDepth.classList.toggle('active');
 })
-
+//family_btn
 let familyBtn = document.querySelector('.family_btn');
 let familyList = document.querySelector('.family_list');
 
 familyBtn.addEventListener('click', function(){
     familyList.classList.toggle('active');
 })
+//mo_family_btn
+let moFamilyBtn = document.querySelector('.mo_family_btn');
+let moFamilyList = document.querySelector('.mo_family_list');
+
+moFamilyBtn.addEventListener('click', function(){
+    moFamilyList.classList.toggle('active');
+})
+
+
+//service_slide
+$('.gallery_1').bxSlider({
+    mode : 'vertical',
+    minSlides: 1,
+    maxSlides: 1,
+    slideWidth: 'auto',
+    slideMargin: 20,
+    ticker: true,
+    speed: 50000
+  });
+  $('.gallery_2').bxSlider({
+    mode : 'vertical',
+    minSlides: 1,
+    maxSlides: 1,
+    slideWidth: 'auto',
+    slideMargin: 20,
+    ticker: true,
+    speed: 50000
+  });
+
+  //mo_service_slide
+$('.mo_gallery_1').bxSlider({
+    mode : 'horizontal',
+    minSlides: 2,
+    maxSlides: 5,
+    slideWidth: 'auto',
+    slideMargin: 20,
+    ticker: true,
+    speed: 50000,
+    infiniteLoop: true
+  });
+  $('.mo_gallery_2').bxSlider({
+    mode : 'horizontal',
+    minSlides: 2,
+    maxSlides: 5,
+    slideWidth: 'auto',
+    slideMargin: 20,
+    ticker: true,
+    speed: 50000,
+    infiniteLoop: true
+  });
