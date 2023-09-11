cbd593550789ee80184676f5fd770526a7c98e7a
sosally <thdud4143@gmail.com>
2023-09-11 18:18:20 +0900

    0911 최종완료

--- vanilla.js
+++ vanilla.js
@@ -1,22 +1,25 @@
 let hamMenuIcon = document.getElementById('ham_menu');
 let navBar = document.querySelector('.hamburger_wrap');
-//안되면 쿼리셀렉터로
+let closeBtn = document.querySelector('.close_right'); 
 
 let navLinks = document.querySelectorAll('.hamburger_nav li');
 
-hamMenuIcon.addEventListener('click', function(){
-    navBar.classList.toggle('active');
-    hamMenuIcon.classList.toggle('fa-times');
-})
-
-
-navLinks.forEach(function(x){
-    x.addEventListener('click', function(){
-        navBar.classList.remove('active');
-        hamMenuIcon.classList.toggle('fa-times');
-    })
+hamMenuIcon.addEventListener('click', function() {
+  navBar.classList.toggle('active'); 
+  hamMenuIcon.classList.toggle('fa-times'); 
 });
 
+closeBtn.addEventListener('click', function() {
+  navBar.classList.remove('active'); 
+  hamMenuIcon.classList.remove('fa-times'); 
+});
+
+navLinks.forEach(function(x) {
+  x.addEventListener('click', function() {
+    navBar.classList.remove('active');
+    hamMenuIcon.classList.remove('fa-times'); 
+  });
+});
 let globalIcon = document.querySelector('.global_icon');
 let globalDepth = document.querySelector('.global_depth');
 
